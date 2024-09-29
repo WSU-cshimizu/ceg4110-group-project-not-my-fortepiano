9c06ac3a2aa38faad96dd1347a7f3f041f5be55e
Alex Shampton <shampton.10@wright.edu>
2024-09-28 22:38:41 -0400

    updated menu, added initial html pages, and added navigation to pages

--- code/main.py
+++ code/main.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_homepage():
-    return(render_template('home.html'))
+    return(render_template('base.html'))
+
+@app.route('/freeplay')
+def render_freeplay():
+    return(render_template('freeplay.html'))
+
+@app.route('/play-music')
+def render_play_music():
+    return(render_template('play_music.html'))
+
+@app.route('/survival')
+def render_survival():
+    return(render_template('survival.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
