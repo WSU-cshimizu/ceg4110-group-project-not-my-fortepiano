f1153eef3a0445674d514562b3d029c7180d8127
Alex Yeoh <yeoh.3@wright.edu>
2024-10-18 16:21:57 -0400

    some maybe working code?

--- code/api/key_press.py
+++ code/api/key_press.py
@@ -1,11 +1,16 @@
 from flask import jsonify, abort
 import fluidsynth
+import mido
 import json
 
+currentNote = 0
 
+
+# currently no way to exit, not sure if its needed
 def key_press(data):
     match data["mode"]:
         case "play_music":
+            # should be initilized when play_music mode is selected
             velocity = 127  # 127 is max for Nice-Steinway-Lite-v3.0.sf2
             fs = fluidsynth.Synth()
             fs.start()
@@ -13,11 +18,33 @@ def key_press(data):
                 "assets/instruments/Nice-Steinway-Lite-v3.0.sf2"
             )  # from https://sites.google.com/site/soundfonts4u/
             fs.program_select(0, sfid, 0, 0)
+
+            # unclear how this sound is passed to the front
             fs.noteon(0, data["key"], velocity)
         case "survival":
             pass
         case "song":
-            pass
+            # should be initilized when a specific song is selected
+            # Open the MIDI file
+            mid = mido.MidiFile("assets/songs/Mary Had a Little Lamb.mid")
+            notes = []
+            # Iterate over tracks
+            for track in mid.tracks:
+                for msg in track:
+                    instruction = str(msg)
+                    if instruction.__contains__("note_off"):
+                        notes.append(instruction[24:26])
+
+            global currentNote
+            print("current note is " + instruction[currentNote])
+            if data["key"] == instruction[currentNote]:
+                currentNote += 1
+                return jsonify(
+                    {"correctNote": True, "nextNote": instruction[currentNote]}
+                )
+            else:
+                return jsonify({"correctNote": False, "nextNote": 0})  # exit the mode
+
         case _:
             abort(400, description="Missing 'mode' field")
 
