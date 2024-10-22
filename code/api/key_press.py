10d7f8cbc7239cfe279cb76e4801beeb48d70449
Alex Yeoh <yeoh.3@wright.edu>
2024-10-22 11:17:46 -0400

    removed unnecessary comments

--- code/api/key_press.py
+++ code/api/key_press.py
@@ -33,7 +33,7 @@ def key_press(data):
                 previousNote = random.randint(53, 67)
                 score += 1
                 return jsonify({"correctNote": True, "nextNote": previousNote})
-            pass
+            
         case "song":
             # should be initilized when a specific song is selected
             # Open the MIDI file
@@ -58,13 +58,3 @@ def key_press(data):
 
         case _:
             abort(400, description="Missing 'mode' field")
-
-    # # # Verify mode is play music or survival
-    # # if data['mode'] == "play_music" or data['mode'] == "survival":
-    # #     # TODO: Create logic for next note
-
-    # #     # Return information to user
-    # #     # TODO: Fill in values
-    #     return jsonify({'correctNote': True, 'nextNote': '12'})
-    # else:
-    #     abort(400, description="Missing 'mode' field")
