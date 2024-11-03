012647c838054059e90b640d9061800c4c0fc1b3
bakedpotato0 <latham.27@wright.edu>
2024-11-03 16:06:22 -0500

    started development of survival score function

--- piano-logic/DBcontrol.py
+++ piano-logic/DBcontrol.py
@@ -2,8 +2,8 @@ import csv
 import pandas as pd
 
 def newScoreFile(songName): #I am making these as functions so hopefully they can be easily reused/integrated
-    emptyScore = {'score': [],
-                  'name': []}
+    emptyScore = {'score': [0, 0, 0],
+                  'name': [" ", " ", " "]}
     newScoreDF = pd.DataFrame(emptyScore)
     newScoreDF.to_csv(songName + "score.csv", index=True)
     
@@ -21,8 +21,8 @@ def resetScores():
     survScore = newScoreFile("surv")
     
     
-resetSongs()
-resetScores()
+#resetSongs() #initial call to create songs.csv
+#resetScores() #initial call to create scores csv files
  
 #function(s) to access and change current song's score.csv
     #access songs.csv
@@ -45,3 +45,27 @@ resetScores()
     #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
     #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
     #only need to maintain/display top 3 scores
+def loadSurvScore(score):
+    currentScores = pd.read_csv("survscore.csv")
+    if (score > currentScores[[0]].iloc[[0]]):
+        currentScores.iloc[[2]] = currentScores.iloc[[1]]
+        currentScores.iloc[[1]] = currentScores.iloc[[0]]
+        currentScores[0].iloc[[0]] = score
+        #input initials into name location
+        initials = input("Enter your initials: ")
+        currentScores[0].iloc[[1]] = initials
+    else:
+        if(score == currentScores[[0]].iloc[[0]] or score > currentScores[[0]].iloc[[1]]):
+            currentScores[[2]] = currentScores.iloc[[1]]
+            currentScores[[1]].iloc = score
+            #input initials into name location
+            initials = input("Enter your initials: ")
+            currentScores[0].iloc[[1]] = initials
+        else:
+            if (score == currentScores[[0]].iloc[[1]] or score > currentScores[[0]].iloc[[2]]):
+                currentScores[[2]].iloc[[0]] = score
+                #input initials into name location
+                initials = input("Enter your initials: ")
+                currentScores[0].iloc[[1]] = initials
+    print(currentScores)
+    currentScores.to_csv("survscore.csv", index=True)
