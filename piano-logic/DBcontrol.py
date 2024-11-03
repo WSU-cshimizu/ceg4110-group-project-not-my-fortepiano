8668c29bc2cd46ad782c816ce5384b4e37b047f9
bakedpotato0 <latham.27@wright.edu>
2024-11-03 14:44:47 -0500

    started database development

--- piano-logic/DBcontrol.py
+++ piano-logic/DBcontrol.py
@@ -25,5 +25,23 @@ resetSongs()
 resetScores()
  
 #function(s) to access and change current song's score.csv
+    #access songs.csv
+    #find correct song's row
+    #get the score csv
+    #bring the score csv into a dataframe
+    #compare top 3 scores with user's current score
+    #nested ifs:
+    #if current score > 1st place score, shift 2nd place to 3rd place, 1st place to 2nd place, prompt user for initials and replace 1st place score
+    #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
+    #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
+    #only need to maintain/display top 3 scores
 
-#function(s) to access and change survscore.csv
+#function(s) to access and change survscore.csv 
+    #access survival score csv
+    #bring the csv into a dataframe
+    #compare top 3 scores with user's current score
+    #nested ifs:
+    #if current score > 1st place score, shift 2nd place to 3rd place, 1st place to 2nd place, prompt user for initials and replace 1st place score
+    #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
+    #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
+    #only need to maintain/display top 3 scores
