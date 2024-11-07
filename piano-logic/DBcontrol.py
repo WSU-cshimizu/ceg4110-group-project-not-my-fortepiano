eda46031a337cd69f2ed2dd7c75f7df82cb26faf
bakedpotato0 <latham.27@wright.edu>
2024-11-07 14:45:46 -0500

    created song score update functions

--- piano-logic/DBcontrol.py
+++ piano-logic/DBcontrol.py
@@ -1,71 +1,112 @@
 import csv
 import pandas as pd
 
-def newScoreFile(songName): #I am making these as functions so hopefully they can be easily reused/integrated
+def newScoreFile(songName): #function to make an "empty" score file
     emptyScore = {'score': [0, 0, 0],
-                  'name': [" ", " ", " "]}
+                  'name': [' ', ' ', ' ']}
     newScoreDF = pd.DataFrame(emptyScore)
-    newScoreDF.to_csv(songName + "score.csv", index=True)
+    newScoreDF.to_csv(songName + 'score.csv', index=False)
     
-def resetSongs():
-    songData = {'songTitle':["Mary Had a Little Lamb", "Down by the station"],
-                'musicFile':["Mary Had a Little Lamb.mid", "Down by the station.mid"],
-                'scoreFile':["MHLLscore.csv", "DBSscore.csv"]
+def resetSongs(): #function to relate song titles, their midi files, and their score files
+    songData = {'songTitle':['Mary Had a Little Lamb', 'Down by the station'],
+                'musicFile':['Mary Had a Little Lamb.mid', 'Down by the station.mid'],
+                'scoreFile':['MHLLscore.csv', 'DBSscore.csv']
                 }
     songFrame = pd.DataFrame(songData)
-    songFrame.to_csv("songs.csv", index=False)
+    songFrame.to_csv('songs.csv', index=False)
 
-def resetScores():
-    songScore1 = newScoreFile("MHLL")
-    songScore2 = newScoreFile("DBS")
-    survScore = newScoreFile("surv")
+def resetScores(): #function to make new score files for the different songs and modes; this is rudimentary and can be called to remake all files with scores set to 0.
+    songScore1 = newScoreFile('MHLL')
+    songScore2 = newScoreFile('DBS')
+    survScore = newScoreFile('surv')
     
+#resetSongs() #initial call to create songs.csv; comment out when game is in use!
+#resetScores() #initial call to create scores csv files; comment out when game is in use!
+
+#functions to access and change current song's score.csv
+def findSongScore(currentSong):
+    #open songs.csv
+    songDir = pd.read_csv('songs.csv')
+    #find the song by its name
+    #find the score file in that row
+    #return the score file's name so we have a parameter to pass to loadSongScore()
+    return(songDir[songDir['songTitle'] == currentSong]['scoreFile'].values[0])
     
-#resetSongs() #initial call to create songs.csv
-#resetScores() #initial call to create scores csv files
- 
-#function(s) to access and change current song's score.csv
-    #access songs.csv
-    #find correct song's row
     #get the score csv
+def loadSongScore(scoreFile, score):
     #bring the score csv into a dataframe
+    currentScores = pd.read_csv(scoreFile)
     #compare top 3 scores with user's current score
     #nested ifs:
-    #if current score > 1st place score, shift 2nd place to 3rd place, 1st place to 2nd place, prompt user for initials and replace 1st place score
-    #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
-    #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
-    #only need to maintain/display top 3 scores
-
-#function(s) to access and change survscore.csv 
-    #access survival score csv
-    #bring the csv into a dataframe
-    #compare top 3 scores with user's current score
-    #nested ifs:
-    #if current score > 1st place score, shift 2nd place to 3rd place, 1st place to 2nd place, prompt user for initials and replace 1st place score
-    #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
-    #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
-    #only need to maintain/display top 3 scores
-def loadSurvScore(score):
-    currentScores = pd.read_csv("survscore.csv")
-    if (score > currentScores[[0]].iloc[[0]]):
-        currentScores.iloc[[2]] = currentScores.iloc[[1]]
-        currentScores.iloc[[1]] = currentScores.iloc[[0]]
-        currentScores[0].iloc[[0]] = score
+    if (score > currentScores.iloc[0, 0]):
+        #if current score > 1st place score, shift 2nd place to 3rd place, 1st place to 2nd place, prompt user for initials and replace 1st place score
+        currentScores.iloc[2, 0] = currentScores.iloc[1, 0]
+        currentScores.iloc[2, 1] = currentScores.iloc[1, 1]
+        currentScores.iloc[1, 0] = currentScores.iloc[0, 0]
+        currentScores.iloc[1, 1] = currentScores.iloc[0, 1]
+        currentScores.iloc[0, 0] = score
         #input initials into name location
         initials = input("Enter your initials: ")
-        currentScores[0].iloc[[1]] = initials
+        currentScores.iloc[0, 1] = initials
     else:
-        if(score == currentScores[[0]].iloc[[0]] or score > currentScores[[0]].iloc[[1]]):
-            currentScores[[2]] = currentScores.iloc[[1]]
-            currentScores[[1]].iloc[[0]] = score
+        if(score == currentScores.iloc[0, 0] or score > currentScores.iloc[1, 0]):
+            #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
+            currentScores.iloc[2, 0] = currentScores.iloc[1, 0]
+            currentScores.iloc[2, 1] = currentScores.iloc[1, 1]
+            currentScores.iloc[1, 0]= score
             #input initials into name location
             initials = input("Enter your initials: ")
-            currentScores[0].iloc[[1]] = initials
+            currentScores.iloc[1, 1] = initials
         else:
-            if (score == currentScores[[0]].iloc[[1]] or score > currentScores[[0]].iloc[[2]]):
-                currentScores[[2]].iloc[[0]] = score
+            if (score == currentScores.iloc[1, 0] or score > currentScores.iloc[2, 0]):
+                #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
+                currentScores.iloc[2, 0] = score
                 #input initials into name location
                 initials = input("Enter your initials: ")
-                currentScores[0].iloc[[1]] = initials
+                currentScores.iloc[2, 1] = initials
+    #only need to maintain/display top 3 scores
     print(currentScores)
-    currentScores.to_csv("survscore.csv", index=True)
+    currentScores.to_csv(scoreFile, index=False)
+
+#function to access and change survscore.csv 
+def loadSurvScore(score):
+    #access survival score csv
+    #bring the csv into a dataframe
+    currentScores = pd.read_csv('survscore.csv')
+    #compare top 3 scores with user's current score
+    #nested ifs:
+    if (score > currentScores.iloc[0, 0]):
+        #if current score > 1st place score, shift 2nd place to 3rd place, 1st place to 2nd place, prompt user for initials and replace 1st place score
+        currentScores.iloc[2, 0] = currentScores.iloc[1, 0]
+        currentScores.iloc[2, 1] = currentScores.iloc[1, 1]
+        currentScores.iloc[1, 0] = currentScores.iloc[0, 0]
+        currentScores.iloc[1, 1] = currentScores.iloc[0, 1]
+        currentScores.iloc[0, 0] = score
+        #input initials into name location
+        initials = input("Enter your initials: ")
+        currentScores.iloc[0, 1] = initials
+    else:
+        if(score == currentScores.iloc[0, 0] or score > currentScores.iloc[1, 0]):
+            #if current score > 2nd place score or current score = 1st place, shift 2nd place to 3rd place, prompt user for initials and replace 2nd place score
+            currentScores.iloc[2, 0] = currentScores.iloc[1, 0]
+            currentScores.iloc[2, 1] = currentScores.iloc[1, 1]
+            currentScores.iloc[1, 0]= score
+            #input initials into name location
+            initials = input("Enter your initials: ")
+            currentScores.iloc[1, 1] = initials
+        else:
+            if (score == currentScores.iloc[1, 0] or score > currentScores.iloc[2, 0]):
+                #if current score > 3rd place score or current score = 2nd place, prompt user for initials and replace 3rd place score
+                currentScores.iloc[2, 0] = score
+                #input initials into name location
+                initials = input("Enter your initials: ")
+                currentScores.iloc[2, 1] = initials
+    #only need to maintain/display top 3 scores
+    print(currentScores)
+    currentScores.to_csv('survscore.csv', index=False)
+    
+#testing function calls;  comment out when game is in use!
+#loadSurvScore(1000)
+#loadSurvScore(500)
+#print(findSongScore("Mary Had a Little Lamb"))
+#loadSongScore(findSongScore("Mary Had a Little Lamb"), 1000)
