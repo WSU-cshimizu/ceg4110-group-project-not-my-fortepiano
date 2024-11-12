a0e256e1c3db080e046fac75810649650909335c
Alex Yeoh <yeoh.3@wright.edu>
2024-11-12 10:59:10 -0500

    better note fix that fixes it for any song

--- code/api/piano/select_song.py
+++ code/api/piano/select_song.py
@@ -14,12 +14,32 @@ def select_song(data):
         for msg in track:
             instruction = str(msg)
             if instruction.__contains__("note_off"):
-                notes.append(str(int(instruction[24:26])+12))
+                notes.append(instruction[24:26])
+                # notes.append(str(int(instruction[24:26])+12))
+    avg = 0
+    shift = 0
+    print(1)
+    for note in notes:
+        avg += int(note)
+    avg = avg / len(notes)
+    print(2)
+    print(avg)
+    while avg < 53 or avg > 67:
+        if avg < 53:
+            shift += 1
+            avg = float(avg) + 12
+        else:
+            shift -= 1
+            avg = float(avg) - 12
+    print(3)
+    for i, note in enumerate(notes):
+        print(i)
+        notes[i] = int(note) + shift * 12
     session["notes"] = notes
     # print(session["notes"])
     # print(data['song'])
 
-    print(session['notes'])
+    print(session["notes"])
 
     return jsonify(
         {"correctNote": True, "nextNote": session["notes"][session["index"]]}
