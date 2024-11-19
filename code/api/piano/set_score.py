5074d90de2f5062868d6d2b8ee29dfd379c65d98
Alex Yeoh <yeoh.3@wright.edu>
2024-11-19 14:40:41 -0500

    added play_music database updating functionality

--- code/api/piano/set_score.py
+++ code/api/piano/set_score.py
@@ -8,6 +8,15 @@ import DBcontrol
 def set_score(data):
     # TODO set score data.name contains name to assign score to
     # print(session["score"])
-    sys.stdin = StringIO(data["name"])
-    DBcontrol.loadSurvScore(session["score"])
-    return jsonify({})
+    match session["mode"]:
+        case "survival":            
+            # sys.stdin = StringIO(data["name"])
+            DBcontrol.loadSurvScore(session["score"],data["name"])
+            return jsonify({})
+        case "play_music":
+            # sys.stdin = StringIO(data["name"])
+            DBcontrol.loadSongScore(session["song"],session["score"],data["name"])
+            # DBcontrol.loadSongScore(session["song"],session["score"])
+            return jsonify({})
+        case _:
+            abort(400, description="Missing 'mode' field")
